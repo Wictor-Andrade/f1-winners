e0fbbb6244d6fba4c3943beefed39408689a97db
wictor.andrade <wictor.andrade@souzs.com>
2025-04-05 01:12:10 +0000

    feat(react): implement new route

---
+++ resources/views/winners.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        @viteReactRefresh
+        @vite(['resources/app/app.css', 'resources/app/winners/page.tsx'])
+    </head>
+    <body class="antialiased bg-gray-100 text-gray-900">
+        <div id="root"></div>
+    </body>
+</html>
